8c339d19b66107c1f0397e582569e1e90c434b3d
JdC <jdc_santos@outlook.com>
2020-02-23 02:51:07 +0000

    atualizando lista de endpoints

--- public/backend/Config/Routes.php
+++ public/backend/Config/Routes.php
@@ -21,17 +21,17 @@
     /**
 	 *	Rotas Usuário
 	 */
+        /* rotas para criação da conta */
+        ClassRoutes::POST('user/register','ControllerUser@add');
+        ClassRoutes::POST('user/confirm-account','ControllerUser@confirm_account');
         
-        ClassRoutes::PUT('user','ControllerUser@edit');
+        /* rota para atualizar dados da conta */
+        ClassRoutes::PUT('user/update','ControllerUser@edit');
 
         /* rotas para redefinir senha*/
         ClassRoutes::POST('user/reset-password-request','ControllerUser@reset_password_request');
-        ClassRoutes::POST('user/reset-password-code','ControllerUser@verify_reset_password_code');
+        ClassRoutes::POST('user/verify-reset-password-code','ControllerUser@verify_reset_password_code');
         ClassRoutes::PUT('user/reset-password','ControllerUser@reset_password');
-        
-        /* rotas para criação da conta */
-        ClassRoutes::POST('user','ControllerUser@add');
-        ClassRoutes::POST('user/confirm-account','ControllerUser@confirm_account');
 
         /*rotas para exclusão da conta (necessário estar logado)*/
 		ClassRoutes::GET('user/delete-account-request','ControllerUser@delete_account_request');

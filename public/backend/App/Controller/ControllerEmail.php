8543cfdf42dfd05209c765bbcb152c7ee3879313
Jhonatancs <jdc_santos@outlook.com>
2020-03-08 23:38:46 -0300

    adicionando metodos para listar e editar tabela email

--- public/backend/App/Controller/ControllerEmail.php
+++ public/backend/App/Controller/ControllerEmail.php
@@ -11,7 +11,44 @@
 
 	use Src\Classes\ClassEmail;
 
-	class ControllerEmail extends Controller{
+	class ControllerEmail extends Controller {
+
+		public function __construct(){
+			parent::__construct();
+		}
+
+		public function list($data){
+			$this->validate_access('adm');
+
+			$emails = EmailDAO::find(new Email($data));
+
+			$emails  = $this->prepare_array($emails);
+
+			$this->render->json($emails);
+		}
+
+		public function edit($data){
+			$this->validate_access('adm');
+
+			unset($data['type']);
+
+			$res = EmailDAO::edit(new Email($data));
+
+			if($res == "success"){
+			
+				$json['status'] = 'success';
+				$json['msg']	= 'Email atualizado com sucesso. ';
+			
+			}else{
+
+				$json['status'] = 'error';
+				$json['msg']	= 'Não foi possivel atualizar o email';
+				$json['msg']   .= '';
+
+			}
+
+			$this->render->json($json);
+		}
 
 		public function send_confirm_account_request($user, $data){
 			return $this->send_email($user,$data,'confirm_account_request');

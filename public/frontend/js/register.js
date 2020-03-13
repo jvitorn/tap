318c76dc90775336772df8f3b4e6c290b8cf8d77
daviskooo <daviferreiradelima.contato@gmail.com>
2020-03-12 21:51:05 -0300

    Adicionando algumas alterações no registro.

---
+++ public/frontend/js/register.js
@@ -0,0 +1,4 @@
+let name = document.querySelector('#name').value;
+let email = document.querySelector('#email').value;
+let password = document.querySelector('#password').value;
+let check = document.querySelector('#checkTerm').value;
